4e64f3c614d365318be0ebbd35ae70815ce6c038
ByOnyx <byonyxmc@gmail.com>
2024-10-09 21:33:13 +0200

    [FEATURE] Ajout d'un système de spike

---
+++ Assets/Scripts/Spike.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour
+{
+    [SerializeField] private float upTime = 2.0f;
+    [SerializeField] private float downTime = 3.0f;
+    [SerializeField] private GameObject spikeModel;
+    private bool isUp = true;
+    private float time = 0f;
+
+    private void Update() {
+        HandleUpAndDown();
+    }
+
+    private void HandleUpAndDown() {
+        time += Time.deltaTime;
+        if (isUp && time > upTime) {
+            isUp = false;
+            time = 0;
+            spikeModel.SetActive(false);
+        }
+        if (!isUp && time > downTime) {
+            isUp = true;
+            time = 0;
+            spikeModel.SetActive(true);
+        }
+    }
+}
