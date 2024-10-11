179a77c5eefbcb4bfa62822f709f364833430824
ByOnyx <byonyxmc@gmail.com>
2024-10-11 14:44:46 +0200

    Correction des déplacements

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,57 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour {
-    private InputAction moveAction;
-    private InputAction jumpAction;
-    public float moveSpeed = 5f;
-    public float jumpForce = 5f;
-    private Rigidbody rb;
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float sensivity = 3.0f;
 
-    private int jumpsRemaining = 2; // Nombre de sauts disponibles (1 saut normal + 1 double saut)
-    private bool isGrounded = false; // Indique si le joueur est au sol
 
-    void OnEnable() {
-        moveAction = GetComponent<PlayerInput>().actions.FindAction("Move", true);
-        jumpAction = GetComponent<PlayerInput>().actions.FindAction("Jump", true);
-        jumpAction.performed += Jump;
-    }
+    public float walkingSpeed = 7.5f;
+    public float runningSpeed = 11.5f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 45.0f;
+    private float _pitch;
+
+    CharacterController characterController;
+    public Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
+    private float cameraVerticalRotation = 0;
+
+
+
+
+    [HideInInspector]
+    public bool canMove = true;
 
     void Start() {
-        rb = GetComponent<Rigidbody>();
+        characterController = GetComponent<CharacterController>();
+
+        // Lock cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    void FixedUpdate() {
-        Vector2 inputVector = moveAction.ReadValue<Vector2>();
-        Vector3 move = new Vector3(inputVector.x, 0, inputVector.y) * moveSpeed * Time.deltaTime;
-        transform.Translate(move, Space.World);
-    }
 
-    void Jump(InputAction.CallbackContext context) {
-        // Si des sauts sont disponibles
-        if (jumpsRemaining > 0) {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            jumpsRemaining--; // Diminue le nombre de sauts restants
+    void Update() {
+
+        // We are grounded, so recalculate move direction based on axes
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        // Press Left Shift to run
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        if (Input.GetButton("Jump") && canMove && characterController.isGrounded) {
+            moveDirection.y = jumpSpeed;
+        } else {
+            moveDirection.y = movementDirectionY;
         }
-    }
 
-    // Détection de la collision avec le sol
-    void OnCollisionEnter(Collision collision) {
-        if (collision.gameObject.CompareTag("Ground")) // Vérifie que c'est bien le sol
-        {
-            isGrounded = true;
-            jumpsRemaining = 2; // Réinitialise les sauts quand on touche le sol
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        if (!characterController.isGrounded) {
+            moveDirection.y -= gravity * Time.deltaTime;
         }
-    }
 
-    // Quand le joueur quitte le sol
-    void OnCollisionExit(Collision collision) {
-        if (collision.gameObject.CompareTag("Ground")) {
-            isGrounded = false; // Indique que le joueur n'est plus au sol
-        }
-    }
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
 
-    void OnDisable() {
-        jumpAction.performed -= Jump;
+
+        float inputX = Input.GetAxis("Mouse X") * sensivity;
+        float inputY = Input.GetAxis("Mouse Y") * sensivity;
+
+        transform.Rotate(Vector3.up, inputX * sensivity * Time.deltaTime);
+        _pitch -= inputY * sensivity * Time.deltaTime;
+        _pitch = Mathf.Clamp(_pitch, -90f, 90f);
+        transform.localEulerAngles = new Vector3(_pitch, transform.localEulerAngles.y, 0f);
     }
 }
